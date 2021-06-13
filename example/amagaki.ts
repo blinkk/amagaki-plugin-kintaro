ffc400c84f284c8ab4b698afdb217fa2bdcbb0de
Jeremy Weinstein <jeremydw@gmail.com>
2021-06-12 18:39:03 -0700

    feat: import many documents

--- example/amagaki.ts
+++ example/amagaki.ts
@@ -10,6 +10,16 @@ interface KintaroModified {
   updated_on_millis: string;
 }
 
+interface KintaroCollection {
+  collection_id: string;
+  db_id: string;
+  mod_info: KintaroModified;
+  published_document_count: 1;
+  repo_id: string;
+  schema_id: string;
+  total_document_count: string;
+}
+
 interface KintaroDocument {
   collection_id: string;
   content_json: string;
@@ -34,48 +44,103 @@ export default async (pod: Pod) => {
     keyFile: 'key.json',
   });
   const client = await kintaro.getClient();
-  const resp = await client.documents.getDocument({
+
+  const items = await client.collections.listCollections({
     repo_id: 'bracket-gmail',
     project_id: 'bracket-gmail',
     use_json: true,
-    document_id: '6189338546143232',
-    collection_id: 'CarouselContent',
   });
-  const data = resp.data as KintaroDocument;
-  const baseContent = flat.flatten(JSON.parse(data.content_json));
 
-  const keysToLocales = {};
-  for (const [key, val] of Object.entries(baseContent)) {
-    keysToLocales[key] = {
-      en: val,
-    };
-  }
+  const collectionIds = items.data.collections.map(item => {
+    return item.collection_id;
+  });
 
-  const locales = data.snapshot_locales;
-  const resps = await Promise.all(
-    locales.map(locale => {
-      return client.documents.getDocument({
+  console.log(`Processing ${collectionIds.length} collections`);
+
+  for (const collectionId of collectionIds) {
+    console.log(`Processing ${collectionId}`);
+    const subItems = await client.documents.listDocuments({
+      repo_id: 'bracket-gmail',
+      project_id: 'bracket-gmail',
+      use_json: true,
+      collection_id: collectionId,
+    });
+    if (!subItems.data.documents) {
+      continue;
+    }
+    console.log(`Processing ${subItems.data.documents.length} docs`);
+    const documentIds = subItems.data.documents.map(item => {
+      return item.document_id;
+    });
+
+    for (const documentId of documentIds) {
+      const resp = await client.documents.getDocument({
         repo_id: 'bracket-gmail',
         project_id: 'bracket-gmail',
         use_json: true,
-        document_id: '6189338546143232',
-        collection_id: 'CarouselContent',
-        locale: locale,
+        document_id: documentId,
+        collection_id: collectionId,
       });
-    })
-  );
+      const data = resp.data as KintaroDocument;
+      const baseContent = flat.flatten(JSON.parse(data.content_json));
 
-  for (const resp of resps) {
-    const data = resp.data as KintaroDocument;
-    const localizedContent = flat.flatten(JSON.parse(data.content_json));
-    for (const [key, val] of Object.entries(localizedContent)) {
-      if (keysToLocales[key]) {
-        keysToLocales[key][data.locale] = val;
-      } else {
-        console.log(`${key} not found.`);
+      const keysToLocales = {};
+      for (const [key, val] of Object.entries(baseContent)) {
+        keysToLocales[key] = {
+          en: val,
+        };
       }
+
+      const locales = data.snapshot_locales;
+      const resps = await Promise.all(
+        locales.map(locale => {
+          return client.documents.getDocument({
+            repo_id: 'bracket-gmail',
+            project_id: 'bracket-gmail',
+            use_json: true,
+            document_id: documentId,
+            collection_id: collectionId,
+            locale: locale,
+          });
+        })
+      );
+
+      for (const resp of resps) {
+        const data = resp.data as KintaroDocument;
+        const localizedContent = flat.flatten(JSON.parse(data.content_json));
+        for (const [key, val] of Object.entries(localizedContent)) {
+          if (keysToLocales[key]) {
+            keysToLocales[key][data.locale] = val;
+          } else {
+            console.log(`${key} not found.`);
+          }
+        }
+      }
+
+      const patterns = [
+        '.label$',
+        '.text$',
+        '_label$',
+        '^headline$',
+        '^next$',
+        '^previous$',
+        '.title$',
+      ];
+
+      const isTranslationString = key => {
+        return key.match(new RegExp(patterns.join('|')));
+      };
+
+      const keysToLocalesToStrings = {};
+      for (const [key, val] of Object.entries(keysToLocales)) {
+        if (isTranslationString(key)) {
+          keysToLocalesToStrings[key] = val;
+        } else {
+          console.log(`Skipped key -> ${key} (${Object.values(val)[0]})`);
+        }
+      }
+
+      await kintaroPlugin.saveLocales(pod, keysToLocalesToStrings);
     }
   }
-
-  console.log(keysToLocales);
 };
--- src/kintaro.ts
+++ src/kintaro.ts
@@ -141,6 +141,61 @@ class KintaroPlugin {
   }
 }
 
+export type KeysToStrings = Record<string, string>;
+export type KeysToLocalesToStrings = Record<string, KeysToStrings>;
+
+/**
+ * Updates the pod's locale files with translations retrieved from the sheet.
+ */
+export async function saveLocales(
+  pod: Pod,
+  keysToLocales: KeysToLocalesToStrings
+) {
+  type Catalog = Record<string, string>;
+  type LocalesToCatalogs = Record<string, Catalog>;
+  const catalogsToMerge: LocalesToCatalogs = {};
+  for (const localesToStrings of Object.values(keysToLocales)) {
+    const baseString = localesToStrings[pod.defaultLocale.id];
+    // No source translation found, skip it.
+    if (!baseString) {
+      continue;
+    }
+    for (const [locale, translatedString] of Object.entries(localesToStrings)) {
+      if (!catalogsToMerge[locale]) {
+        catalogsToMerge[locale] = {};
+      }
+      catalogsToMerge[locale][baseString] = translatedString;
+    }
+  }
+  // TODO: Replace this code once the locale catalog format within Amagaki is
+  // stable, and there are built-in methods for updating catalogs and writing
+  // translations.
+  for (const [localeId, catalog] of Object.entries(catalogsToMerge)) {
+    const locale = pod.locale(localeId);
+    let contentToWrite;
+    if (!pod.fileExists(locale.podPath)) {
+      contentToWrite = yaml.dump(
+        {translations: catalog},
+        {
+          schema: pod.yamlSchema,
+        }
+      );
+    } else {
+      const existingContent = pod.readYaml(locale.podPath);
+      Object.assign(existingContent['translations'], catalog);
+      const content = yaml.dump(existingContent, {
+        schema: pod.yamlSchema,
+      });
+      contentToWrite = content;
+    }
+    await pod.builder.writeFileAsync(
+      pod.getAbsoluteFilePath(locale.podPath),
+      contentToWrite
+    );
+    console.log(`Saved -> ${locale.podPath}`);
+  }
+}
+
 export const register = (
   pod: Pod,
   authPluginOptions: GoogleAuthPluginOptions
